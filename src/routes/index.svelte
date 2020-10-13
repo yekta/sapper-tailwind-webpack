b85d664ac1180a64d0ce67ffa01ad4651a624d1b
yekta <ngyekta@gmail.com>
2020-10-13 17:09:27 +0300

    Upload from local

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>Sapper project template</title>
+</svelte:head>
+
+<h1 class="text-4xl font-extrabold text-blue-500 text-center">Tailwind On!</h1>
+
+<figure class="w-full flex flex-col items-center mt-4">
+  <img class="w-64 max-w-full" alt="Success Kid" src="successkid.jpg" />
+  <figcaption class="mt-4">Have fun with Sapper!</figcaption>
+</figure>
